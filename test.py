3d76ae465bb6db28a5387c1a5c464d56b3bd2612
李雪松 <li.xuesong@synitalent.com>
2017-08-26 13:42:37 +0800

    @Update 2017-08-26 函数的定义与实现

--- test.py
+++ test.py
@@ -1,11 +1,24 @@
 #!/bin/usr/
 #-*-coding : UTF-8-*-
-def pay(x,n)
+# 计算乘方
+def pay(x,n):
     s=1
-    while x < 1:
+    while n > 0:
         n = n -1
         s = s * x
     return s
 
 zui=pay(5,2)
-print(zui)
+print zui
+
+# 计算面积函数
+def area(width, height):
+    return width * height
+ 
+def print_welcome(name):
+    print("Welcome", name)
+
+print_welcome("Runoob")
+w = 4
+h = 5
+print("width =", w, " height =", h, " area =", area(w, h))
