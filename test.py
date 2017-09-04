01ca31a03310ad34c628ab1ff07b3a503c23af4f
李雪松 <li.xuesong@synitalent.com>
2017-09-04 15:12:05 +0800

    @2017-09-04 一些自带的属性与装饰器用法

--- test.py
+++ test.py
@@ -1,24 +1,40 @@
 #!/bin/usr/
 #-*-coding : UTF-8-*-
-# 计算乘方
-def pay(x,n):
-    s=1
-    while n > 0:
-        n = n -1
-        s = s * x
-    return s
+# # 计算乘方
+# def pay(x,n):
+#     s=1
+#     while n > 0:
+#         n = n -1
+#         s = s * x
+#     return s
 
-zui=pay(5,2)
-print zui
+# zui=pay(5,2)
+# print zui
 
-# 计算面积函数
-def area(width, height):
-    return width * height
+# # 计算面积函数
+# def area(width, height):
+#     return width * height
  
-def print_welcome(name):
-    print("Welcome", name)
+# def print_welcome(name):
+#     print("Welcome", name)
 
-print_welcome("Runoob")
-w = 4
-h = 5
-print("width =", w, " height =", h, " area =", area(w, h))
+# print_welcome("Runoob")
+# w = 4
+# h = 5
+# print("width =", w, " height =", h, " area =", area(w, h))
+
+class Chain(object):
+    
+    def __init__(self, path=''):
+        self._path = path
+
+    def __getattr__(self, path):
+        return Chain('%s/%s' % (self._path, path))
+    # __call__=__getattr__
+
+    def __str__(self):
+        return self._path
+
+    __repr__ = __str__
+a=Chain()
+print(a.user.jiji)
